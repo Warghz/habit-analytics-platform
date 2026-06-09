8e9cd9ef88658af957eaeaccdfdc63ce91bb6a96
Wargh <dencebyr771@gmail.com>
2026-06-09 19:48:15 +0300

    feat(users.py): add self-service user API

--- app/routers/users.py
+++ app/routers/users.py
@@ -16,59 +16,23 @@ router = APIRouter(
 )
 
 
-@router.post("/", status_code=status.HTTP_201_CREATED, response_model=UserOut)
-async def create_user(
-        user: UserCreate,
-        session: AsyncSession = Depends(get_session)
+@router.get('/me', response_model=UserOut)
+async def get_me(
+        current_user: User = Depends(get_current_user)
 ):
-    new_user = User(
-        email=user.email,
-        hashed_password=hash_password(user.password)
-    )
-
-    session.add(new_user)
-    await session.commit()
-    await session.refresh(new_user)
-
-    return new_user
+    return current_user
 
 
-@router.get("/{user_id}", response_model=UserOut)
-async def get_user(
-        user_id: int,
-        session: AsyncSession = Depends(get_session)
+@router.delete("/me", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(
+        session: AsyncSession = Depends(get_session),
+        current_user: User = Depends(get_current_user)
 ):
     result = await session.execute(
-        select(User).where(User.id == user_id)
+        select(User).where(User.id == current_user.id)
     )
+    user = result.scalar_one_or_none()
 
-    find_user = result.scalar_one_or_none()
-
-    if find_user is None:
-        raise HTTPException(status_code=404, detail="User not found.")
-
-    return find_user
-
-
-@router.get("/", response_model=List[UserOut])
-async def get_users(session: AsyncSession = Depends(get_session)):
-
-    result = await session.execute(select(User))
-    users = result.scalars().all()
-
-    return users
-
-
-@router.delete("/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_user(
-        user_id: int,
-        session: AsyncSession = Depends(get_session)
-):
-    result = await session.execute(select(User).where(User.id == user_id))
-    del_user = result.scalar_one_or_none()
-
-    if del_user is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found.")
-
-    await session.delete(del_user)
-    await session.commit()
+    if user:
+        await session.delete(current_user)
+        await session.commit()
