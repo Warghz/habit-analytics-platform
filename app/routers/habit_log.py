8396e1f88711b78fbd8bc2e6b7ac0581e2449ac8
Wargh <dencebyr771@gmail.com>
2026-06-11 11:06:43 +0300

    feat(habit_log.py): add 1 more method about habit history

--- app/routers/habit_log.py
+++ app/routers/habit_log.py
@@ -2,12 +2,12 @@ from fastapi import APIRouter
 from fastapi.params import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.database import get_session
-from core.dependencies import get_current_user
+from app.core.dependencies import get_current_user
 from app.models.users import User
-from services.habit_log import HabitLogService
+from app.services.habit_log import HabitLogService
 
 router = APIRouter(
-    prefix='habit_log',
+    prefix='/habit_logs',
     tags=['habit_log']
 )
 
@@ -27,4 +27,18 @@ async def get_streak(
 ):
     return {
         "streak": await HabitLogService.get_streak(session, habit_id, current_user.id)
+    }
+
+@router.get("/{habit_id}/history")
+async def get_history(
+        habit_id: int,
+        session: AsyncSession = Depends(get_session),
+        current_user: User = Depends(get_current_user)
+):
+    return {
+        'dates': await HabitLogService.get_history(
+            session,
+            habit_id,
+            current_user.id
+        )
     }
