d6fc8f283f99993f1b1700be288d7bf4264f1de4
Wargh <dencebyr771@gmail.com>
2026-06-11 11:05:55 +0300

    feat(__init__.py): add habit_logs router

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -2,3 +2,4 @@ from .habits import router as habits_router
 from .users import router as users_router
 from .auth import router as auth_router
 from .admin import router as admin_router
+from .habit_log import router as habit_log_router
