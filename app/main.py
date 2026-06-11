c9aa66426eb0991db6a3a411b247334de6ca513d
Wargh <dencebyr771@gmail.com>
2026-06-11 10:59:17 +0300

    feat(main.py): add 1 new router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.routers import users_router, habits_router, auth_router, admin_router
+from app.routers import users_router, habits_router, auth_router, admin_router, habit_log_router
 from app.test import router as test_router
 from .database import engine, get_session
 from contextlib import asynccontextmanager
@@ -30,6 +30,7 @@ app.include_router(users_router)
 app.include_router(auth_router)
 app.include_router(test_router)
 app.include_router(admin_router)
+app.include_router(habit_log_router)
 
 app.add_exception_handler(HabitAlreadyExistsError, habit_already_exists_handler)
 app.add_exception_handler(HabitNotFoundError, habit_not_found_handler)
